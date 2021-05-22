c2fc54e6e5de1275ea65b9177d02fcb11e01f0d9
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-05-22 09:11:53 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://movie-7702a-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        'https://movie-7702a-default-rtdb.firebaseio.com/tasks.json',
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
